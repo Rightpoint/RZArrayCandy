c70eb1ec972d12f163e0e5cc5b2e2424730c507b
Nick Donaldson <nick.donaldson@raizlabs.com>
2014-05-28 13:54:06 -0400

    Initial Commit

---
+++ RZFunctionalContainers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RZFunctionalContainers
+//
+//  Created by Nick Donaldson on 5/28/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
